30f1084cb567269efba35a4784faaa27b1b4e680
SWanstall <s.wanstall.13@gmail.com>
2024-08-16 22:48:19 +0100

    Update gun.gd

--- gun.gd
+++ gun.gd
@@ -6,12 +6,12 @@ signal shoot(bullet, direction, location, speed)
 
 var Bullet = preload("res://bullet.tscn")
 
-@export var bullet_speed = 10
+@export var bullet_speed = 1000
 
 
 func _process(delta):
 	
 	if Input.is_action_just_pressed("click") and !fired:
-		shoot.emit(Bullet, -global_transform.basis.z, global_position, bullet_speed)
+		shoot.emit(Bullet, -global_transform.basis.z, global_position, bullet_speed*delta)
 		print(-global_transform.basis.z)
 
