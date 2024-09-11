a6184509090c26e2622d15b9b37325e2a02760ac
SWanstall <s.wanstall.13@gmail.com>
2024-09-11 19:22:06 +0100

    Made menu appear on Tab press and vice versa

--- inventory.gd
+++ inventory.gd
@@ -1,11 +1,20 @@
 extends Control
 
+var menu_open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	visible = false
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_focus_next") and menu_open == false:
+		visible = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+		menu_open = true
+	elif Input.is_action_just_pressed("ui_focus_next") and menu_open == true:
+		visible = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		menu_open = false
